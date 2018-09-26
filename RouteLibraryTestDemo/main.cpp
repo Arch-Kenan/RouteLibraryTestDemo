4e264d2a8834edd07d6c943d0fe8d7bae98756fe
Kenan <2513084541@qq.com>
2018-09-26 17:09:27 +0800

    test

--- RouteLibraryTestDemo/main.cpp
+++ RouteLibraryTestDemo/main.cpp
@@ -9,40 +9,27 @@
 #include <fstream>
 #include <thread>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <functional>
 
 
 //inlcude other project
 #include "compositePatten.h"
-
-
-int strcpy(char* des, char* src)
-{
-
-}
-
-
 using namespace std;
+
+
+
+
 //平曲线测试主程序
 int main()
 {
-	char chrs[20];
-	int a = 123;
-	sprintf(chrs, "%d ", a);
-	string   strs(chrs);
-	char chrs1[20];
-	int b = 576;
-	sprintf(chrs1, "%d ", b);
-	string   strs1(chrs1);
-	strs += strs1;
 
-	stringstream   ss(strs);
-	int  word;
-	while (ss >> word)
-		cout << ++word << endl;
-
-
+	int b = 8;
+	int a = 3;
+	double  c =  b / a;
+	std::cout << c;
+	
 
 
 
---
+++ RouteLibraryTestDemo/stlOP.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+//char chrs[20];
+//int a = 123;
+//sprintf(chrs, "%d ", a);
+//string   strs(chrs);
+//char chrs1[20];
+//int b = 576;
+//sprintf(chrs1, "%d ", b);
+//string   strs1(chrs1);
+//strs += strs1;
+//
+//stringstream   ss(strs);
+//int  word;
+//while (ss >> word)
+//cout << ++word << endl;
+
+
+int strcpy(char* des, char* src)
+//{
+//	return 0;
+//}
+//
+//bool  pred(int num)
+//{
+//	return num < 12;
+//}
+//list<int>    arrs;
+//for (int i = 0; i < 9; i++)
+//{
+//	arrs.push_back(i * 3);
+//}
+//std::remove_if(arrs.begin(), arrs.end(), pred);
+////arrs.remove_if(pred);
+//for (auto n : arrs)
+//{
+//	std::cout << n << "   ";
+//}
